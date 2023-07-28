9ef04f9f8796f42546b1e5149835603414825498
Dean Lofts <dean@deanlofts.xyz>
2023-07-29 00:21:53 +1000

    add sign in with github functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # You can have the root of your site routed with "root"
+  root to: 'home#index'
 end
