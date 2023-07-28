92bb335fdff3ae90d1b4557daff31d56659cf2bd
Dean Lofts <dean@deanlofts.xyz>
2023-07-28 18:05:00 +1000

    set active job to resque

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RailsFullCiCd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Set Resque as the backend for ActiveJob
+    config.active_job.queue_adapter = :resque
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
