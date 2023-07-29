90b65bd5babb36f8f611cc344fe79cd7be0513ee
Dean Lofts <dean@deanlofts.xyz>
2023-07-29 14:05:59 +1000

    some progress

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Allow connections from docker network
+  config.web_console.permissions = '172.0.0.0/8'
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
