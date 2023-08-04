a4a635adca7392c4139d64928be85689621edbc0
Dean Lofts <dean@deanlofts.xyz>
2023-08-04 22:08:44 +1000

    failed experiment admin panel

--- db/migrate/20230804085619_add_trackable_fields_to_admin_users.rb
+++
@@ -1,9 +0,0 @@
-class AddTrackableFieldsToAdminUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :admin_users, :sign_in_count, :integer, limit: 4
-    add_column :admin_users, :current_sign_in_at, :datetime
-    add_column :admin_users, :last_sign_in_at, :datetime
-    add_column :admin_users, :current_sign_in_ip, :string
-    add_column :admin_users, :last_sign_in_ip, :string
-  end
-end
